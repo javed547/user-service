dc097b8d09390a75053844bdb1aca8a96215b208
Mohd Javed Khan <jal90javed@gmail.com>
2019-09-08 12:58:10 +0530

    integrated spring security with custom authentication

--- src/main/java/com/javed/aws/applications/config/CustomAuthenticationProvider.java
+++ src/main/java/com/javed/aws/applications/config/CustomAuthenticationProvider.java
@@ -1,16 +1,25 @@
 package com.javed.aws.applications.config;
 
+import com.javed.aws.applications.model.Login;
+import com.javed.aws.applications.model.User;
+import com.javed.aws.applications.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserService userService;
 
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String userName = authentication.getName();
@@ -19,7 +28,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         if (authorizedUser(userName, password)) {
             List<GrantedAuthority> grantedAuths = new ArrayList<GrantedAuthority>();
             grantedAuths.add(() -> {
-                return "AUTH_USER";
+                return "ROLE_ADMIN";
             });
             Authentication auth = new UsernamePasswordAuthenticationToken(userName, password, grantedAuths);
             System.out.println(auth.getAuthorities());
@@ -31,9 +40,12 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     private boolean authorizedUser(String userName, String password) {
         System.out.println("username is :" + userName + " and password is " + password);
-        if ("Chandan".equals(userName) && "Chandan".equals(password))
+        User user = userService.validateUser(new Login(userName, password));
+        if (user.getUsername().equals(userName) && user.getPassword().equals(password)){
             return true;
-        return false;
+        } else {
+            return false;
+        }
     }
 
     public boolean supports(Class<?> authentication) {
